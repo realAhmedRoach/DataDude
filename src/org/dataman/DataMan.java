36b8a8aaaf8567e40de18e1e6a076bc9a904b520
theTechnoKid <thetechnokid11@gmail.com>
2014-11-10 19:57:06 -0500

    Updated DataMan

--- src/org/dataman/DataMan.java
+++ src/org/dataman/DataMan.java
@@ -1,21 +1,36 @@
 package org.dataman;
 
+import java.util.*;
+
 import javax.swing.*;
 
 import org.dataman.memory.MemoryManager;
 
 public final class DataMan {
 
+	public static final HashMap<String, String> fileClassNames;
+	
+	public static final String VERSION = "DataMan Beta 0.1";
+	
+	public static final MemoryManager manager;
+	
+	static {
+		String pack = "org.dataman.nodes.";
+		fileClassNames = new HashMap<String, String>();
+		fileClassNames.put("txt", pack+"TextNode");
+		manager = new MemoryManager();
+		
+	}
+	
 	private DataMan() {
 	}
 
-	
 	public static void openConsole() {
 		
 	}
 	
 	public MemoryManager getMemoryManager() {
-		return new MemoryManager();
+		return manager;
 	}
 	
 	public static String getFolder() {
@@ -32,7 +47,6 @@ public final class DataMan {
 		fs.showOpenDialog(null);
 		if(fs.getSelectedFile() != null)
 			return fs.getSelectedFile().getAbsolutePath();
-			fs = null;
 		return "null";
 	}
 
