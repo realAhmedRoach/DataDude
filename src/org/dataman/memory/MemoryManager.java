b19a2621d4d3b65e04d19491639571f6ce669a35
theTechnoKid <thetechnokid11@gmail.com>
2014-11-27 13:45:59 -0500

    Added Thread Support
    Printed out the memory available.

--- src/org/dataman/memory/MemoryManager.java
+++ src/org/dataman/memory/MemoryManager.java
@@ -1,20 +1,37 @@
 package org.dataman.memory;
 
-public class MemoryManager {
+public class MemoryManager implements Runnable{
 	
 	private Runtime r;
+	final static long MEGABYTE = 1024L * 1024L;
+	final static long KILOBYTE = 1024L;
 	
 	public MemoryManager() {
 		r = Runtime.getRuntime();
 	}
 	
-	public long getAvailableMemory() {
-		return r.totalMemory();
+	public void run() {
+		while(true) {
+			System.out.println("Mem: " + getFreeMemory() + "kB");
+			//if ()
+			try {
+				Thread.sleep(5000);
+			} catch (InterruptedException e) {e.printStackTrace();}
+		}
+	}
+	
+	public long getMaxMemory() {
+		return (r.maxMemory()/MEGABYTE);
+	}
+	
+	public long getTotalMemory() {
+		return (r.totalMemory()/MEGABYTE);
 	}
 	public long getUsedMemory() {
-		return r.freeMemory() - getAvailableMemory();
+		return ((r.freeMemory() - getTotalMemory())/MEGABYTE);
 	}
 	public long getFreeMemory() {
-		return r.freeMemory();
+		return (r.freeMemory()/KILOBYTE);
 	}
 }
+
