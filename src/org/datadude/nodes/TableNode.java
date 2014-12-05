9af7d49cc241db9cc36da63001b7bc4d90fc109d
theTechnoKid <theTechnoKid11@gmail.com>
2014-12-05 17:19:03 -0500

    Made TableNode work

--- src/org/datadude/nodes/TableNode.java
+++ src/org/datadude/nodes/TableNode.java
@@ -1,7 +1,3 @@
-/**
- * 
- */
-
 package org.datadude.nodes;
 
 import javax.swing.*;
@@ -15,9 +11,28 @@ public class TableNode extends Node {
 	JTable mainTable;
 
 	public TableNode() {
-		mainTable = new JTable();
+		int[] rac = this.askRowsAndColumns();
+		mainTable = new JTable(rac[0], rac[1]);
+		mainTable.setVisible(true);
+		this.add(mainTable);
 		this.setEnabled(true);
 		this.setVisible(true);
+	}
+	
+	
+	/**
+	 * 
+	 * @return An array with two values, the amount of rows and columns.
+	 */
+	private int[] askRowsAndColumns() {
+		int[] rac = new int[2];
+		try {
+		rac[0] = Integer.parseInt(JOptionPane.showInputDialog("How many rows?"));
+		rac[1] = Integer.parseInt(JOptionPane.showInputDialog("How many columns?"));
+		} catch (NumberFormatException e) {
+			JOptionPane.showMessageDialog(this, "Your're supposed to put in a number!");
+		}
+		return rac;
 	}
 
 }
