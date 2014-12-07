13da46cf4b5c0d49c847ff355ae677b3eca0a5bb
theTechnoKid <thetechnokid11@gmail.com>
2014-12-07 09:49:03 -0500

    Added GPLv3 Licence. Renamed DataManFile to DataDudeFile

--- src/org/datadude/datamanaging/Database.java
+++ src/org/datadude/datamanaging/Database.java
@@ -1,3 +1,21 @@
+/**
+    DataDude is a data managing applicationdesigned to have mny types of data in one application
+    Copyright (C) 2015  Ahmed R. (theTechnoKid)
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package org.datadude.datamanaging;
 
 import java.io.*;
@@ -9,7 +27,7 @@ public class Database {
 	Directory directory;
 	Pointer dirPointer;
 
-	ArrayList<DataManFile> files;
+	ArrayList<DataDudeFile> files;
 	
 	/**
 	 * 
@@ -19,7 +37,7 @@ public class Database {
 		directory = new Directory();
 		dirPointer = new Pointer(Pointer.DIRECTORY_POINTER);
 		dirPointer.setCurrFile(new File(path));
-		files = new ArrayList<DataManFile>();
+		files = new ArrayList<DataDudeFile>();
 				
 	}
 
@@ -28,7 +46,7 @@ public class Database {
 	 * @param theFile file to be added
 	 * @return {@code true} if added successfully
 	 */
-	public boolean addFile(DataManFile theFile) {
+	public boolean addFile(DataDudeFile theFile) {
 		files.add(theFile);
 		
 		return true;
