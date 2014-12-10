94898ef3bd15ed0c13ae7f2bd9a5e4d800e1c44f
theTechnoKid <thetechnokid11@gmail.com>
2014-12-09 19:07:17 -0500

    Commented property printing

--- src/org/datadude/Main.java
+++ src/org/datadude/Main.java
@@ -30,12 +30,12 @@ public class Main {
 	public static void main(String[] args) {
 		os = System.getProperty("os.name");
 		System.out.println(os);
-		System.out.println(System.getProperty("os.version"));
+		/*System.out.println(System.getProperty("os.version"));
 		System.out.println(System.getProperty("os.arch"));
 		System.out.println(System.getProperty("java.home"));
 		System.out.println(System.getProperty("user.name"));
 		System.out.println(System.getProperty("user.home"));
-		System.out.println(System.getProperty("user.dir"));
+		System.out.println(System.getProperty("user.dir"));*/
 
 		checkForFolders();
 
