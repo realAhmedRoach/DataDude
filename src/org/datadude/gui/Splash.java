d5c7111c2cd02a944b335dfabed1069c9f198668
theTechnoKid <thetechnokid11@gmail.com>
2014-12-10 14:05:03 -0500

    Completed Splash.

--- src/org/datadude/gui/Splash.java
+++ src/org/datadude/gui/Splash.java
@@ -3,36 +3,41 @@ package org.datadude.gui;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.image.BufferedImage;
+import java.io.File;
 
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 
 public class Splash extends JFrame {
 	private static final long serialVersionUID = 3979584611778054086L;
 
 	private BufferedImage splash;
-	
+
 	/**
 	 * Create the frame.
 	 */
 	public Splash() {
-		setOpacity(.5f);
+		// setOpacity(.5f);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		setSize(500,500);
+		setSize(500, 500);
 		setLocationRelativeTo(null);
-		
-		setLocation(0, 0);
+
+		try {
+			splash = ImageIO.read(new File("images/dd_splash.png"));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 		// Set undecorated
 		setUndecorated(true);
+		
 		// Apply a transparent color to the background
 		// This is ALL important, without this, it won't work!
 		setBackground(new Color(0, 255, 0, 0));
-
-		// This is where we get sneaky, basically where going to 
-		// supply our own content pane that does some special painting
-		// for us
 		getContentPane().setBackground(Color.BLACK);
 		setLayout(new BorderLayout());
-
+		add(new JLabel(new ImageIcon(splash)));
 		setVisible(true);
 	}
 }
