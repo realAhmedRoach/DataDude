e2761a70f8f528be5d411e59fcb29559d36ca216
theTechnoKid <thetechnokid11@gmail.com>
2015-01-06 08:07:45 -0500

    Added Updater for 0.0.3

---
+++ src/org/datadude/datamanaging/Updater.java
@@ -0,0 +1,60 @@
+/*******************************************************************************
+ *     DataDude is a data managing application designed to have many types of data in one application
+ *     Copyright (C) 2015  Ahmed R. (theTechnoKid)
+ *
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *******************************************************************************/
+package org.datadude.datamanaging;
+
+import java.io.*;
+import java.net.URL;
+import java.util.ArrayList;
+
+/**
+ * @author theTechnoKid
+ * Updater for DataDude
+ * This checks if the version on the update 
+ * database is the same as the current version.
+ * If not, it downloads the file.
+ */
+public class Updater {
+	private static final String update = "https://thetechnokid.github.io/DataDude/update.html";
+	
+	public static String getVersion() throws Exception {
+		String version = getData().get(0);
+		return version.substring(version.indexOf("[version]")+9, version.indexOf("[/version]"));
+	}
+	
+	public static String getWhatsNew() throws Exception {
+		String wasnew = getData().get(1);
+		return wasnew.substring(wasnew.indexOf("[new]")+5, wasnew.indexOf("[/new]"));
+	}
+	
+	public static String getDownload() throws Exception {
+		String downLoc = getData().get(2);
+		return downLoc.substring(downLoc.indexOf("[download]")+10, downLoc.indexOf("[/download]"));
+	}
+	
+	private static ArrayList<String> getData() throws Exception {
+		URL url = new URL(update);
+		InputStream html = url.openStream();
+		BufferedReader r = new BufferedReader(new InputStreamReader(html));
+		
+		ArrayList<String> s = new ArrayList<String>();
+		String line = null;
+		while((line = r.readLine())!=null) 
+			s.add(line);
+		return s;
+	}
+}
