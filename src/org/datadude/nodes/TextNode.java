c12792164a0b2d19d2d2d09713b574e45cc6ab2f
theTechnoKid <theTechnoKid11@gmail.com>
2015-01-14 12:41:55 -0500

    Reverted to TextNode's default font

--- src/org/datadude/nodes/TextNode.java
+++ src/org/datadude/nodes/TextNode.java
@@ -18,27 +18,14 @@
 
 package org.datadude.nodes;
 
-import java.awt.BorderLayout;
-import java.awt.Font;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
 
-import javax.swing.JFileChooser;
-import javax.swing.JLabel;
-import javax.swing.JMenuItem;
-import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
-import javax.swing.KeyStroke;
+import javax.swing.*;
 
-import org.datadude.Login;
-import org.datadude.datamanaging.DataDudeFile;
+import org.datadude.*;
+import org.datadude.datamanaging.*;
 
 public class TextNode extends BasicNode implements ActionListener {
 	private static final long serialVersionUID = 6657L;
@@ -51,7 +38,6 @@ public class TextNode extends BasicNode implements ActionListener {
 		init();
 		pad = " ";
 		ta = new JTextArea(); // textarea
-		ta.setFont(new Font("DokChampa", Font.PLAIN, 14));
 		scpane = new JScrollPane(ta); // scrollpane and add textarea to
 										// scrollpane
 
