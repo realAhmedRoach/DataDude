928be04eeb14d3f7fd3c9787ed06a524dd75f44d
theTechnoKid <theTechnoKid11@gmail.com>
2015-02-11 10:39:28 -0500

    Bump version 0.0.4-rc1

--- src/org/datadude/DataDude.java
+++ src/org/datadude/DataDude.java
@@ -38,7 +38,7 @@ public final class DataDude {
 	static JFrame preloader;
 	private static JLabel text;
 
-	public static final String VERSION = "DataDude 0.0.3 Patch 2";
+	public static final String VERSION = "DataDude 0.0.4-rc1";
 
 	public static final MemoryManager manager;
 
