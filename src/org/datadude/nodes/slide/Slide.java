67b9535054ffb8c6539ba52820842574c65c2df9
theTechnoKid <theTechnoKid11@gmail.com>
2015-02-24 20:52:35 -0500

    Made slide implement Sreializable

--- src/org/datadude/nodes/slide/Slide.java
+++ src/org/datadude/nodes/slide/Slide.java
@@ -1,15 +1,18 @@
 package org.datadude.nodes.slide;
 
 import java.awt.image.BufferedImage;
+import java.io.Serializable;
 
 import javax.swing.JPanel;
 
-public class Slide {
+public class Slide implements Serializable {
+	private static final long serialVersionUID = 7119022762742506719L;
+
 	private String title;
 	private BufferedImage image;
 	private String text;
 	private JPanel customContent;
-	
+
 	public Slide(String _title, String _text) {
 		title = _title;
 		text = _text;
